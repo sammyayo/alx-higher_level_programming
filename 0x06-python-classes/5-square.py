7fc36fe3a19dd5a8d4d21f9906b6d9eddebc5fcc
sammyayo <99363328+sammyayo@users.noreply.github.com>
2022-06-21 23:33:04 +0100

    Update 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,62 +1,47 @@
 #!/usr/bin/python3
+"""
+Create a Class Square with:
+- size proprety
+- method of area and method of print_square
+- getters & setters.
+"""
+
+
 class Square:
-    """Square Class
-    A Square Class
-    """
+    """Class - Square"""
 
     def __init__(self, size=0):
-        """__init__
-        The __init__ method initializes the size value of the square.
-        Attributes:
-            size (:obj:`int`, optional): The size of the square.
-        Raises:
-            TypeError: If `size` type is not `int`.
-            ValueError: If `size` is less than `0`.
-        """
+        """Constructor of a Square with the size"""
+        if (type(size) is not int):
+            raise (TypeError("size must be an integer"))
+        elif (size < 0):
+            raise (ValueError("size must be >= 0"))
+        else:
+            self.__size = size
 
-        if type(size) is not int:
-            raise TypeError('size must be an integer')
+    def area(self):
+        """Method to get the area of the Square"""
+        return (self.__size ** 2)
 
-        if size < 0:
-            raise ValueError('size must be >= 0')
-
-        self.size = size
+    def my_print(self):
+        """Method to print a Square"""
+        if (self.__size == 0):
+            print()
+        else:
+            for rows in range(self.__size):
+                print("#" * self.__size)
 
     @property
     def size(self):
-        return self.__size
+        """Getter of the private attribute size"""
+        return (self.__size)
 
     @size.setter
-    def size(self, size):
-        """__init__
-        The size setter method update the size value of the square.
-        Attributes:
-            size (:obj:`int`): The new size of the square.
-        Raises:
-            TypeError: If `size` type is not `int`.
-            ValueError: If `size` is less than `0`.
-        """
-
-        if type(size) is not int:
-            raise TypeError('size must be an integer')
-
-        if size < 0:
-            raise ValueError('size must be >= 0')
-
-        self.__size = size
-
-    def area(self):
-        """Returns the current square area
-        """
-        return self.__size ** 2
-
-    def my_print(self):
-        if self.__size == 0:
-            print()
-            return None
-
-        for i in range(1, self.area() + 1):
-            print('#', end='')
-
-            if i % self.__size == 0 and i > 0:
-                print()
+    def size(self, value):
+        """Setter for the size private attribute"""
+        if (type(value) is not int):
+            raise (TypeError("size must be an integer"))
+        elif (value < 0):
+            raise (ValueError("size must be >= 0"))
+        else:
+            self.__size = value
